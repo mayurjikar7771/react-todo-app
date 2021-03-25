71b76c66e00622b530a68ae8cf27ae0f0ca2d930
mayurjikar7771 <mrmayurjikar7771>
2021-03-26 01:40:25 +0530

    Create Router,Logout Page & Router Not Found

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-
-import Login from './Login/LoginContainer';
 import './style.css';
 
-import Dashboard from './Dashboard';
-import Profile from './Profile/ProfileContainer';
-ReactDOM.render(<Profile />, document.getElementById('root'));
+import AppRouter from './router/AppRouter';
+// import Login from './Login/LoginContainer';
+// import Dashboard from './Dashboard';
+// import Profile from './Profile/ProfileContainer';
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
